46edc170079abda26c5b708ddbeddf5bdb3993d6
horsicq <horsicq@gmail.com>
2021-10-27 18:15:09 +0200

    Fix: 2021-10-27

--- xabstracttableview.cmake
+++ xabstracttableview.cmake
@@ -1,6 +1,11 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
+include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialogtextinfo.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../XShortcuts/xshortcuts.cmake)
+
 set(XABSTRACTTABLEVIEW_SOURCES
+    ${XSHORTCUTS_SOURCES}
+    ${DIALOGTEXTINFO_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xabstracttableview.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xdevicetableview.cpp
 )
--- xdatetimeeditx.cmake
+++ xdatetimeeditx.cmake
@@ -1,1 +1,5 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+set(XDATETIMEEDITX_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/xdatetimeeditx.cpp
+    )
