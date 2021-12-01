f9b2ad149401786320e523e25c8d921c4b4347bf
horsicq <horsicq@gmail.com>
2021-12-01 17:40:19 +0100

    Fix: 2021-12-01

--- xabstracttableview.h
+++ xabstracttableview.h
@@ -149,6 +149,7 @@ public:
     static QFont getMonoFont(qint32 nFontSize=-1);
 
     qint64 getSelectionInitOffset();
+
     void setCurrentBlock(qint64 nOffset,qint64 nSize);
     bool isOffsetInCurrentBlock(qint64 nOffset);
 
