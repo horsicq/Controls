48299e1dc77de6a3c4c4996d2e0ba7292bbce069
horsicq <horsicq@gmail.com>
2022-06-11 01:06:52 +0200

    Fix: 2022-06-11

--- xdevicetableview.cpp
+++ xdevicetableview.cpp
@@ -194,7 +194,7 @@ bool XDeviceTableView::isReplaced(qint64 nOffset,qint32 nSize)
     return XBinary::_isReplaced(nOffset,nSize,&g_listReplaces);
 }
 
-void XDeviceTableView::goToAddress(XADDR nAddress, bool bShort, bool bAprox)
+void XDeviceTableView::goToAddress(XADDR nAddress,bool bShort,bool bAprox)
 {
     qint64 nOffset=XBinary::addressToOffset(getMemoryMap(),nAddress);
     _goToOffset(nOffset,false,bShort,bAprox); // TODO Check
