625b302b22d1765cde7b038cca8b015ebd393af3
horsicq <horsicq@gmail.com>
2022-08-18 18:57:34 +0200

    Fix: 2022-08-18

--- xlineedithex.cpp
+++ xlineedithex.cpp
@@ -341,6 +341,7 @@ qint32 XLineEditHEX::getWidthFromMode(QWidget *pWidget,XLineEditHEX::MODE mode)
     qint32 nSymbolWidth=getSymbolWidth(pWidget);
     qint32 nResult=nSymbolWidth*8;
 
+    // TODO Check
     if(mode==MODE_8)
     {
         nResult=nSymbolWidth*4;
