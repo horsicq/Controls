d67c5b0d798f335ef06fb41dd17f119d6aed3a2f
horsicq <horsicq@gmail.com>
2024-12-17 00:50:40 +0100

    Update file(s): 2024-12-17

--- xcomboboxex.cmake
+++ xcomboboxex.cmake
@@ -1,6 +1,7 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(XCOMBOBOXEX_SOURCES
+    ${XCOMBOBOXEX_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/subclassofqstyleditemdelegate.cpp
     ${CMAKE_CURRENT_LIST_DIR}/subclassofqstyleditemdelegate.h
     ${CMAKE_CURRENT_LIST_DIR}/xcomboboxex.cpp
