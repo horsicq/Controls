6cdfc2261630c53cfd02e5118795656c1141727f
horsicq <horsicq@gmail.com>
2024-12-19 08:17:00 +0100

    Update file(s): 2024-12-19

--- xtableview.cmake
+++ xtableview.cmake
@@ -1,6 +1,7 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(XTABLEVIEW_SOURCES
+    ${XTABLEVIEW_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xtableview.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xtableview.h
     ${CMAKE_CURRENT_LIST_DIR}/xheaderview.cpp
