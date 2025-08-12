3f91ed0638be61d7c6a4219b3acc54129064f4b3
horsicq <horsicq@gmail.com>
2025-08-12 20:48:32 +0200

    Update file(s): 2025-08-12

--- xlineedithex.cmake
+++ xlineedithex.cmake
@@ -9,4 +9,8 @@ set(XLINEEDITHEX_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/xlineeditvalidator.h
     ${CMAKE_CURRENT_LIST_DIR}/xlineedithex.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xlineedithex.h
+    ${CMAKE_CURRENT_LIST_DIR}/xlineedit_num.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xlineedit_num.h
+    ${CMAKE_CURRENT_LIST_DIR}/xlineedit_num_validator.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xlineedit_num_validator.h
     )
--- xlineedithex.pri
+++ xlineedithex.pri
@@ -4,11 +4,15 @@ DEPENDPATH += $$PWD
 HEADERS += \
     $$PWD/dialogbits8.h \
     $$PWD/xlineedithex.h \
+    $$PWD/xlineedit_num.h \
+    $$PWD/xlineedit_num_validator.h \
     $$PWD/xlineeditvalidator.h
 
 SOURCES += \
     $$PWD/dialogbits8.cpp \
     $$PWD/xlineedithex.cpp \
+    $$PWD/xlineedit_num.cpp \
+    $$PWD/xlineedit_num_validator.cpp \
     $$PWD/xlineeditvalidator.cpp
 
 FORMS += \
