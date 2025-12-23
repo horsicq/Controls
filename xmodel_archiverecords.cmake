2ecf6d18436299a62f97c2a5edae30b5be9f89e0
horsicq <horsicq@gmail.com>
2025-12-23 18:23:30 +0100

    feat: add CMake file for xmodel archive records

---
+++ xmodel_archiverecords.cmake
@@ -0,0 +1,9 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+set(XMODEL_ARCHIVERECORDS_SOURCES
+    ${XMODEL_ARCHIVERECORDS_SOURCES}
+    ${CMAKE_CURRENT_LIST_DIR}/xmodel.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xmodel.h
+    ${CMAKE_CURRENT_LIST_DIR}/xmodel_archiverecords.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xmodel_archiverecords.h
+)
