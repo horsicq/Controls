bc14bd8b38b92332d8e97921ff55d6222d2fe816
horsicq <horsicq@gmail.com>
2026-03-10 20:27:03 +0100

    feat: add XFControls module with dialog and view components

---
+++ XFControls/xfcontrols.cmake
@@ -0,0 +1,24 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+if (NOT DEFINED XFMODEL_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../../Formats/xfmodel.cmake)
+    set(XFCONTROLS_SOURCES ${XFCONTROLS_SOURCES} ${XFMODEL_SOURCES})
+endif()
+
+set(XFCONTROLS_SOURCES
+    ${XFCONTROLS_SOURCES}
+    ${CMAKE_CURRENT_LIST_DIR}/xftableview.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xftableview.h
+    ${CMAKE_CURRENT_LIST_DIR}/xftreeview.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xftreeview.h
+    ${CMAKE_CURRENT_LIST_DIR}/xfstringdialog.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xfstringdialog.h
+    ${CMAKE_CURRENT_LIST_DIR}/xfvaluedialog.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xfvaluedialog.h
+    ${CMAKE_CURRENT_LIST_DIR}/xfsizedialog.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xfsizedialog.h
+    ${CMAKE_CURRENT_LIST_DIR}/xfflagsdialog.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xfflagsdialog.h
+    ${CMAKE_CURRENT_LIST_DIR}/xflistdialog.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xflistdialog.h
+)
