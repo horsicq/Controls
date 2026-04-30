293876407ef8e295561d60f52a330951590f5686
horsicq <horsicq@gmail.com>
2026-04-30 19:17:28 +0200

    feat: add include and source references to xmodel_archiverecords.cmake

--- xmodel_archiverecords.cmake
+++ xmodel_archiverecords.cmake
@@ -1,7 +1,10 @@
+include(${CMAKE_CURRENT_LIST_DIR}/xmodel.cmake)
+
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(XMODEL_ARCHIVERECORDS_SOURCES
     ${XMODEL_ARCHIVERECORDS_SOURCES}
+    ${XMODEL_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xmodel_archiverecords.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xmodel_archiverecords.h
 )
